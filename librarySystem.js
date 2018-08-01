d95216f9a9e6941acd8c56db53f5bcde5ef32d1a
Brad DeWindt <betawind@gmail.com>
2018-07-31 21:01:00 -0700

    adding librarySystem and tests

---
+++ librarySystem.js
@@ -0,0 +1,52 @@
+(function(){
+  var libraryStorage = {};
+
+  function librarySystem(libraryName, dependencies, callback) {
+    
+    // Throw exception if incorrect syntax
+    if (arguments.length < 1 || arguments.length > 3) {
+      throw new RangeError ('Incorrect syntax.  The correct syntax is librarySystem(libraryName, dependencies, callback)')
+    }
+    // Store library if more than one argument
+    if (arguments.length > 1) {
+      // Handle when the second argument is a function instead of an array
+      if(!Array.isArray(dependencies) && typeof dependencies == "function") {
+        callback = depenencies;
+        dependencies = []; 
+      } else if (!Array.isArray(dependencies)){
+        // Throw exception if the second argument isn't an array or a function
+        throw new TypeError ('Second argument must be a string array.')
+      }
+      
+      // Throw exception if any of the dependencies aren't strings
+      for (var i = 0 ; i < dependencies.length ; i++) {
+        if (typeof dependencies[i] !== "string") {
+          throw new TypeError ('All elements in the array must be strings')
+        }
+      }
+      
+      // Add the library to storage
+      libraryStorage[libraryName] = {
+        dependencies: dependencies,
+        library: callback
+      };
+      return;
+    } 
+    
+    // Throw exception if the library doesn't exist
+    if (!(libraryName in libraryStorage)) {
+      throw new ReferenceError ('Library [' + libraryName + '] does not exist.' );
+    }
+    
+    var returnLibrary = libraryStorage[libraryName];
+    var depResults = [];
+  
+    for (var i = 0 ; i < returnLibrary.dependencies.length ; i++) {
+      depResults.push(librarySystem(returnLibrary.dependencies[i]));
+    }
+
+    return returnLibrary.library.apply(null, depResults);
+  }
+
+  window.librarySystem = librarySystem;
+})();
